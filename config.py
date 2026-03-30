385238c2ab07e45ab8635a857d5811a3354bf051
manojakm149-spec <manojakm149>
2026-03-30 19:36:34 +0530

    Fixed config mismatch

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
 
 # ARMS Portal
+ARMS_URL = "https://arms.sse.saveetha.com"
 ARMS_USERNAME = os.environ.get("ARMS_USERNAME", "")
 ARMS_PASSWORD = os.environ.get("ARMS_PASSWORD", "")
 
